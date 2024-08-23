8054806db6a167eba8a0953d670706edd228359c
Rafael Chaves <rchaves@gradle.com>
2024-08-23 16:10:02 -0300

    Revert c4f6510d1cce947fa9168b58b0b0ad3888cc7cb0 Update assertions to reflect new default for parallel load

--- platforms/core-configuration/configuration-cache/src/integTest/groovy/org/gradle/internal/cc/impl/ConfigurationCacheParallelStoreIntegrationTest.groovy
+++ platforms/core-configuration/configuration-cache/src/integTest/groovy/org/gradle/internal/cc/impl/ConfigurationCacheParallelStoreIntegrationTest.groovy
@@ -26,7 +26,7 @@ class ConfigurationCacheParallelStoreIntegrationTest extends AbstractConfigurati
 
         then:
         output.contains("[org.gradle.configurationcache] saving state nodes in parallel")
-        output.contains("[org.gradle.configurationcache] reading state nodes in-line")
+        output.contains("[org.gradle.configurationcache] reading state nodes in parallel")
     }
 
     def "parallel store may be opted out"() {
@@ -38,18 +38,20 @@ class ConfigurationCacheParallelStoreIntegrationTest extends AbstractConfigurati
 
         then:
         output.contains("[org.gradle.configurationcache] saving state nodes in-line")
-        output.contains("[org.gradle.configurationcache] reading state nodes in-line")
+        // reading is always in parallel
+        output.contains("[org.gradle.configurationcache] reading state nodes in parallel")
     }
 
-    def "parallel load may be opted in"() {
+    def "parallel load may be opted out"() {
         given:
         settingsFile.createFile()
 
         when:
-        configurationCacheRun("help", "-d", "-Dorg.gradle.configuration-cache.internal.parallel-load=true")
+        configurationCacheRun("help", "-d", "-Dorg.gradle.configuration-cache.internal.parallel-load=false")
 
         then:
         output.contains("[org.gradle.configurationcache] saving state nodes in parallel")
-        output.contains("[org.gradle.configurationcache] reading state nodes in parallel")
+        // reading is always in parallel
+        output.contains("[org.gradle.configurationcache] reading state nodes in-line")
     }
 }
